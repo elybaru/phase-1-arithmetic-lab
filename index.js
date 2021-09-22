8eb8bb15688b23f6483d3e00616c4a14aabfd45c
Ely Karr <elykarr@gmail.com>
2021-09-21 20:10:09 -0700

    commit tree d0ded92692a0ef1a855e36a826a7c1b0a8ce8967

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
+
+let random = Math.floor(Math.round(Math.random()+1))
+
+let num3 = 13
+let num4 = 9
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,20); 
